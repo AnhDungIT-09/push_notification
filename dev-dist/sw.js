f221a421fd3eee0ae59605e46fe64a512198a3f6
AnhDungIT-09 <ledinhdung.net@gmail.com>
2025-07-17 16:04:40 +0700

    1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.haa9nbl2ncg"
+    "revision": "0.6bdk48v2pr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
